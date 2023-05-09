5539f42f037fa038c277e1df9e61255937ddcc2f
B DURGA SHANKAR <109042249+durgaxcodes@users.noreply.github.com>
2023-05-09 10:39:58 +0530

    Add files via upload

---
+++ Social Profile/index.js
@@ -0,0 +1,8 @@
+// Add JavaScript code here
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  btn.innerHTML = "Following";
+  btn.style.backgroundColor = "rgb(255, 255, 255)";
+  btn.style.color = "black";
+  btn.style.border = "0.5px solid rgb(167, 166, 166)";
+});
