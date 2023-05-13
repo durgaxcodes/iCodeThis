11e9c2ddf8b6cbf1e2e258275c09f767ff3896e2
B DURGA SHANKAR <109042249+durgaxcodes@users.noreply.github.com>
2023-05-13 17:19:33 +0530

    Add files via upload

---
+++ Metamask/index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+button.addEventListener("click", function () {
+  button.innerHTML = "Following";
+  button.style.backgroundColor = "#e9e3e6";
+  button.style.color = "black";
+});
